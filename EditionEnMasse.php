bdb3b161fe4ae5e7bbace277b8f566c9ee15ea5b
Logan GHILARDI <ghilardilogan@gmail.com>
2019-10-31 11:14:48 +0100

    teeest

---
+++ EditionEnMasse.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edition en masse</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="navbar navbar-inverse">
+			<a class="navbar-brand" href="maquette.php">Accueil</a>
+			<ul class="nav navbar-nav">
+				<li><a href="Exercices.php">Exercices</a></li>
+				<li><a href="Fonction.php">Fonction</a></li>
+				<li><a href="Motif.php">Motif</a></li>
+				<li><a href="Relève&Détachement.php">Relève/Détachement</a></li>
+				<li><a href="EditionEnMasse.php">Edition en masse</a></li>
+			</ul>
+		</div>
+	</div>
+		
+	
+
+</body>
+</html>
